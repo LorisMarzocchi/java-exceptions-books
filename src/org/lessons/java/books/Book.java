71001d5ea33b201d3fb08dbd6409e38ae49eecfe
LorisMarzocchi <ml77182a@gmail.com>
2023-10-25 01:15:25 +0200

    add book

--- src/org/lessons/java/books/Book.java
+++ src/org/lessons/java/books/Book.java
@@ -1,2 +1,80 @@
-package org.lessons.java.books;public class Book {
+package org.lessons.java.books;
+
+public class Book {
+    private String titolo;
+    private int numeroPagine;
+    private String autore;
+    private String editore;
+
+    public Book(String titolo, int numeroPagine, String autore, String editore) {
+        this.titolo = setTitolo(titolo);
+        this.numeroPagine = setNumeroPagine(numeroPagine);
+        this.autore = setAutore(autore);
+        this.editore = setEditore(editore);
+    }
+    @Override
+    public String toString() {
+        return "Libro: " +
+                "Titolo='" + titolo + '\'' +
+                ", Numero di Pagine=" + numeroPagine +
+                ", Autore='" + autore + '\'' +
+                ", Editore='" + editore + '\''
+                ;
+    }
+
+// SETTER
+    public String setTitolo(String titolo) {
+        if (titolo != null && !titolo.isEmpty()) {
+            this.titolo = titolo;
+        } else {
+            throw new IllegalArgumentException("il titolo non puo essere vuoto");
+        }
+        return titolo;
+    }
+
+    public int setNumeroPagine(int numeroPagine) {
+        if (numeroPagine > 0) {
+            this.numeroPagine = numeroPagine;
+        } else {
+            throw new IllegalArgumentException("il numero di pagine deve essere maggiore di 0");
+        }
+        return numeroPagine;
+    }
+
+    public String setAutore(String autore) {
+        if (autore != null && !autore.isEmpty()) {
+            this.autore = autore;
+        } else {
+            throw new IllegalArgumentException("il nome dell'autore non puo essere vuoto");
+        }
+        return autore;
+    }
+
+    public String setEditore(String editore) {
+        if (editore != null && !editore.isEmpty()) {
+            this.editore = editore;
+        } else {
+            throw new IllegalArgumentException("il nome dell'editore non puo essere vuoto");
+        }
+        return editore;
+    }
+
+
+
+    // GETTER
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public String getEditore() {
+        return editore;
+    }
 }
--- src/org/lessons/java/books/Main.java
+++ src/org/lessons/java/books/Main.java
@@ -1,2 +1,56 @@
-package org.lessons.java.books;public class Main {
+package org.lessons.java.books;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Quanti libri vuoi inserire? ");
+        int nLibri = Integer.parseInt(scan.nextLine());
+
+        Book[] catalogoLibri = new Book[nLibri];
+
+        int indice = 0;
+
+        while (indice < catalogoLibri.length){
+            try {
+                System.out.println("inserisci i dati del libro: " + (indice + 1));
+
+                System.out.println("Titolo: ");
+                String titolo = scan.nextLine();
+
+                System.out.println("Numero pagine: ");
+                int numeroPagine = scan.nextInt();
+                scan.nextLine();
+
+                System.out.println("autore: ");
+                String autore = scan.nextLine();
+
+                System.out.println("Editore: ");
+                String editore = scan.nextLine();
+
+                catalogoLibri[indice] = new Book(titolo, numeroPagine, autore, editore);
+                indice++;
+
+            }catch (InputMismatchException e) {
+                System.out.println("Invalid number");
+
+            }catch (NumberFormatException e) {
+                System.out.println("Inserisci un numero valido per il numero di pagine!");
+            }catch (IllegalArgumentException e) {
+                System.out.println("Errore: " + e.getMessage());
+            }
+        }
+
+        System.out.println("Stampa libri");
+        for (Book p : catalogoLibri) {
+            System.out.println("\n" + p);
+        }
+
+
+    scan.close();
+    }
+
 }
